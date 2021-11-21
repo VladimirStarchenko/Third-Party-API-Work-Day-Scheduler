4f065d6d3d9ec2ac4be96c23f2939d31647a17c5
Vladimir Starchenko <vladimistar@gmail.com>
2021-11-21 17:26:39 -0500

    curren time reference for color coding made

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,13 @@ function displayTime() {
 $(document).ready(function () {
   displayTime();
 });
+// display time (by hours) in slots starting from 9am to 5pm (business hours)
+// t is designated as the start numerical value for my time blocks
+//used in the for loop
+function listOfTime() {
+  var t = 9;
+
+  // used as current time by hour for the timeblocks to reference to for color coding
+  var currentTime = moment().hour();
+  console.log(currentTime);
+}
